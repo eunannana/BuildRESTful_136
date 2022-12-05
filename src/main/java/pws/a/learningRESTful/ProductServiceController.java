8c9944e0d73d3cd4ab8291d1151125afbdec1415
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-12-05 10:25:14 +0700

    adding comment for each commands on ProductServiceController.java

--- src/main/java/pws/a/learningRESTful/ProductServiceController.java
+++ src/main/java/pws/a/learningRESTful/ProductServiceController.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// declare package
 package pws.a.learningRESTful;
 
+// declare import
 import model.Product;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,48 +22,82 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author eunannana
  */
+// declare the controller with name RestController
 @RestController
-
+// declare ProductServiceController class
 public class ProductServiceController {
+
+    // declare the static method for the first data
     private static Map<String, Product> productRepo = new HashMap<>();
-    static{
+
+    static {
+        // declare data 1
         Product honey = new Product();
+        // set ID data 1
         honey.setId("1");
+        // set name data 1
         honey.setName("Honey");
+        // declare data value
         productRepo.put(honey.getId(), honey);
-        
+
+        // declare data 2
         Product almond = new Product();
+        // set ID data 2
         almond.setId("2");
+        // set name data 2
         almond.setName("Almond");
+        // declare data value 
         productRepo.put(almond.getId(), almond);
     }
-    
-@RequestMapping(value = "/products")
-public ResponseEntity<Object> getProducts() {
-return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
-}
 
-@RequestMapping(value = "/products", method = RequestMethod.POST)
- public ResponseEntity<Object> createProduct(@RequestBody Product product){
-    if(productRepo.containsKey(product.getId()))
-        return new ResponseEntity<>("Product is already exist, please make a new one", HttpStatus.CONFLICT);
-    productRepo.put(product.getId(), product);
-    return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
- }
- 
- @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
- public  ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product){
-     if(!productRepo.containsKey(id))
-         return new ResponseEntity<>("Product is not found", HttpStatus.NOT_FOUND);
-     productRepo.remove(id);
-     product.setId(id);
-     productRepo.put(id, product);
-     return new ResponseEntity<>("Product is updated successfully", HttpStatus.OK);
- }
- 
- @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
- public ResponseEntity<Object> delete( @PathVariable("id") String id){
-     productRepo.remove(id);
-     return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);     
- }
+    // declare RequestMapping for URL
+    @RequestMapping(value = "/products")
+    // declare getProducts class
+    public ResponseEntity<Object> getProducts() {
+        // return method for getProducts with value and HttpStatus
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+
+    // declare RequestMapping for URL and POSTMETHOD
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    // declare createProduct class 
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        // adding if condition when adding the already exist product
+        if (productRepo.containsKey(product.getId())) {
+            // return method for createProduct that already exist with message and HttpStatus
+            return new ResponseEntity<>("Product is already exist, please make a new one", HttpStatus.CONFLICT);
+        }
+        // declare data value
+        productRepo.put(product.getId(), product);
+        // return method for Success createProduct with message and HttpStatus
+        return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+    }
+
+    // declare RequestMapping for URL and PUT METHOD
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    // declare updateProduct class
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+        // adding if condition when the product is not found to be updated
+        if (!productRepo.containsKey(id)) {
+            // return method for updateProduct that is not found with message and HttpStatus
+            return new ResponseEntity<>("Product is not found", HttpStatus.NOT_FOUND);
+        }
+        // remove data by ID
+        productRepo.remove(id);
+        // declare data value
+        product.setId(id);
+        productRepo.put(id, product);
+        // return method for Success updateProduct with message and Status
+        return new ResponseEntity<>("Product is updated successfully", HttpStatus.OK);
+    }
+
+    // declare RequestMapping for URL and DELETE METHOD
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    // declare deleteProduct class
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id) {
+        // delete data by ID
+        productRepo.remove(id);
+        // return method for Success deleteProduct
+        return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
+    }
 }
