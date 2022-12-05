171d26af1816500544623f529b5ae7df7dd2d806
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-12-05 10:30:23 +0700

    adding comment for each commands on Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -2,29 +2,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// declare model package
 package model;
 
 /**
  *
  * @author eunannana
  */
+// declare Product class
 public class Product {
+    // declare varaible "ID" with String data type
     private String id;
+    // declare varaible "name" with String data type
     private String name;
 
+    // declare getId method
     public String getId() {
+        // return for getId
         return id;
     }
 
+    // declare setID method with id variable
     public void setId(String id) {
+        // declare the ID
         this.id = id;
     }
 
+    // declare getName method
     public String getName() {
+        // return for getName
         return name;
     }
 
+    // declare setName method with name variable
     public void setName(String name) {
+        // declare the name
         this.name = name;
     }
     
